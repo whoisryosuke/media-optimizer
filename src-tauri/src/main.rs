39377fcdd11d4c7ec2ebfeb940fcd396fddc3ed9
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2024-05-24 14:44:58 -0700

    :zap: resize image too

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use rimage::{config::{Codec, EncoderConfig}, error::EncoderError, image::{DynamicImage, ImageResult, RgbaImage}, Decoder, Encoder};
+use rimage::{config::{Codec, EncoderConfig, ResizeConfig, ResizeType}, error::EncoderError, image::{DynamicImage, ImageResult, RgbaImage}, Decoder, Encoder};
 use std::fs::File;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -20,9 +20,14 @@ fn greet(name: &str) -> String {
     let file = File::create("output.png").expect("Failed to create file");
 
     // Configure encoder with settings like file type, compression, etc
+    let resize_config = ResizeConfig::new(ResizeType::Lanczos3)
+        .with_width(800)
+        .with_height(600);
+
     let config = EncoderConfig::new(Codec::Png)
         .with_quality(10.0)
-        .unwrap();
+        .expect("Quality didn't work")
+        .with_resize(resize_config);
     let encoder = Encoder::new(file, image).with_config(config);
 
     // Run encoder
